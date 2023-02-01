f8278e75f0014d81d34717d727e6b5f6e619808a
Walter Nuccio <walter.nuccio@edreamsodigeo.com>
2023-02-01 13:04:42 +0100

    [FF-1543] First test

---
+++ model-layer/src/test/java/com/edreamsodigeo/boardingpass/airobotcheckingateway/FirstTest.java
@@ -0,0 +1,13 @@
+package com.edreamsodigeo.boardingpass.airobotcheckingateway;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
+
+public class FirstTest {
+
+    @Test
+    public void firstTest() {
+        assertEquals(1, 1);
+    }
+}
