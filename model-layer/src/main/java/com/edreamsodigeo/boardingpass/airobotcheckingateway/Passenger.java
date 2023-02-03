f319a93491a6ca275abe859ea191733fc2e5b95e
Walter Nuccio <walter.nuccio@edreamsodigeo.com>
2023-02-03 09:32:45 +0100

    [FF-1543] Add default values to Passenger

--- model-layer/src/main/java/com/edreamsodigeo/boardingpass/airobotcheckingateway/Passenger.java
+++ model-layer/src/main/java/com/edreamsodigeo/boardingpass/airobotcheckingateway/Passenger.java
@@ -1,4 +1,11 @@
 package com.edreamsodigeo.boardingpass.airobotcheckingateway;
 
 public class Passenger {
+    private final String citizenships = "";
+    private final int adults = 0;
+    private final int children = 0;
+    private final int infants = 0;
+
+    public Passenger() {
+    }
 }
