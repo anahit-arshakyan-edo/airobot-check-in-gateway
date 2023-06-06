748063ecd97288f44f909acdf0f5b7d5860e59a3
Anahit Arshakyan <anahit.arshakyan@odigeo.com>
2023-06-06 16:17:12 +0000

    Merged in master (pull request #40)
    FF-1721 remove Reason mapper to the api
    
    * FF-1721 remove Reason mapper to the api
    
    * FF-1721 remove Reason mapper to the api
    
    * FF-1721 Merge conflict
    
    * FF-1721 Merge conflict
    
    * FF-1721 Merge conflict
    
    
    Approved-by: Francisco Apesteguía

--- domain/src/main/java/com/edreamsodigeo/boardingpass/airobotcheckingateway/application/request/checkin/itinerary/Status.java
+++ domain/src/main/java/com/edreamsodigeo/boardingpass/airobotcheckingateway/application/request/checkin/itinerary/Status.java
@@ -57,6 +57,5 @@ public class Status {
         public String description() {
             return description;
         }
-
     }
 }
--- framework/web-app-adapter/src/main/java/com/edreamsodigeo/boardingpass/airobotcheckingateway/webapp/controller/CheckInStatusResponseMapper.java
+++ framework/web-app-adapter/src/main/java/com/edreamsodigeo/boardingpass/airobotcheckingateway/webapp/controller/CheckInStatusResponseMapper.java
@@ -37,14 +37,13 @@ public class CheckInStatusResponseMapper {
     private Status mapStatus(com.edreamsodigeo.boardingpass.airobotcheckingateway.application.request.checkin.itinerary.Status status) {
 
         Status.Code statusCode = null;
-        Status.Reason statusReason = null;
-
 
         switch (status.code()) {
         case FAILED:
             statusCode = Status.Code.FAILED;
             break;
-        case PENDING: case INITIALIZED:
+        case PENDING:
+        case INITIALIZED:
             statusCode = Status.Code.PENDING;
             break;
         case SUCCEED:
@@ -52,35 +51,8 @@ public class CheckInStatusResponseMapper {
             break;
         }
 
-        if (status.reason() != null) {
-            switch (status.reason()) {
-            case BOOKING_NOT_FOUND:
-                statusReason = Status.Reason.BOOKING_NOT_FOUND;
-                break;
-            case PASSENGER_NOT_FOUND:
-                statusReason = Status.Reason.PASSENGER_NOT_FOUND;
-                break;
-            case MISSING_TRAVEL_DOCUMENT_INFO:
-                statusReason = Status.Reason.MISSING_TRAVEL_DOCUMENT_INFO;
-                break;
-            case FLIGHT_CANCELED:
-                statusReason = Status.Reason.FLIGHT_CANCELED;
-                break;
-            case FLIGHT_ALREADY_FLOWN:
-                statusReason = Status.Reason.FLIGHT_ALREADY_FLOWN;
-                break;
-            case ONLY_AIRPORT_CHECK_IN:
-                statusReason = Status.Reason.ONLY_AIRPORT_CHECK_IN;
-                break;
-            case OTHER:
-                statusReason = Status.Reason.OTHER;
-                break;
-            }
-        }
-
         return Status.builder()
                 .withCode(statusCode)
-                .withReason(statusReason)
                 .build();
     }
 
